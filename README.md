da65312238ffbd2ba31dcd234eb9973f54c9bbfc
wanglixing <wanglx03@fenbi.com>
2019-09-09 17:58:13 +0800

    init

---
+++ README.md
@@ -0,0 +1,30 @@
+# PRISM-VUE-DOC
+
+> 用于可以写Vue Demo的 markdown 环境，inspired by [You-need-to-know-css](https://lhammer.cn/You-need-to-know-css/#/zh-cn/)
+
+## 相关依赖
+
+1. docsify 文档md -> 静态网站 的工具，需全局安装
+2. vuep 可以在md文档中直接写 Vue 组件代码的工具，直接显示效果
+3. prism 一个精致的 markdown 文档主题
+4. gitalk 评论回复插件，复用
+
+## 项目用途
+
+1. 用于前端团队交流`Vue`代码片段，写技术分享md使用
+2. 用于前端团队沉淀技术，积累踩坑经验
+3. 用于团队约定流程和代码规范之类的文档位置，方便新人融入团队
+4. 可以作为wiki使用，轻量方便，对前端友好
+
+## 项目特点
+
+1. 使用了定制主题的`markdown`，UI和交互都用了比较成熟的方案
+2. 已经本地引入了大部分插件，不依赖CDN，方便内网部署
+3. 引入了搜索插件，方便作为知识库来使用
+4. 拥有评论能力，方便留言交流。
+
+
+
+## license
+
+[996ICU](https://github.com/l-hammer/You-need-to-know-css/blob/master/LICENSE) © 2017-present, LHammer
---
+++ index.js
@@ -0,0 +1,78 @@
+/**
+ * Docsify config
+ */
+gitalkConfig = {
+  clientID: "8b7cffeb4ae285e0e3e4",
+  clientSecret: "58d06f5220e953d6dac7d6723daf225177cc7bb3",
+  repo: "prism-vue-doc",
+  owner: "wlx200510",
+  admin: ["wlx200510"],
+  perPage: 20,
+  language: "en",
+  labels: ["💬Gitalk"],
+  pagerDirection: "last",
+  distractionFreeMode: false
+};
+window.$docsify = {
+  name: "prism-vue-doc",
+  repo: "https://github.com/wlx200510/prism-vue-doc",
+  auto2top: true,
+  loadNavbar: true,
+  loadSidebar: true,
+  mergeNavbar: true,
+  subMaxLevel: 2,
+  homepage: "README.md",
+  ga: "UA-122081516-1",
+  search: {
+    noData: {
+      "/zh-cn/": "找不到结果!",
+      "/": "No results!"
+    },
+    paths: "auto",
+    placeholder: {
+      "/zh-cn/": "搜索",
+      "/": "Search"
+    }
+  },
+  plugins: [
+    function(hook, vm) {
+      hook.beforeEach(function(html) {
+        var url =
+          "https://github.com/wlx200510/prism-vue-doc/blob/master/" +
+          vm.route.file;
+        var editHtml = "[📝 EDIT DOCUMENT](" + url + ")\n";
+
+        return editHtml + html;
+      });
+
+      hook.doneEach(function() {
+        var label, domObj, main, divEle, gitalk;
+        label = vm.route.path.split("/").pop();
+        domObj = Docsify.dom;
+        main = domObj.getNode("#main");
+
+        /**
+         * render gittalk
+         */
+        if (vm.route.path.includes("zh-cn")) {
+          gitalkConfig.language = "zh-CN";
+        }
+        Array.apply(
+          null,
+          document.querySelectorAll("div.gitalk-container")
+        ).forEach(function(ele) {
+          ele.remove();
+        });
+        divEle = domObj.create("div");
+        divEle.id = "gitalk-container-" + label;
+        divEle.className = "gitalk-container";
+        divEle.style = "width: " + main.clientWidth + "px; margin: 0 auto 20px;";
+        domObj.appendTo(domObj.find(".content"), divEle);
+        gitalk = new Gitalk(
+          Object.assign(gitalkConfig, { id: !label ? "home" : label })
+        );
+        gitalk.render("gitalk-container-" + label);
+      });
+    }
+  ]
+};
